3c7ee373fac86cedeb290775e32c5adc927e68ed
Mike Kerslake <theonlymikeever@gmail.com>
2017-10-27 22:14:35 -0400

    switch over to request-promise-native for api calls

--- server/api/recipe.js
+++ server/api/recipe.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const request = require('request')
+const request = require('request-promise-native')
 
 
 const appId = process.env.appId || null
@@ -7,15 +7,18 @@ const appKey = process.env.appKey || null
 
 router.get('/', (req, res, next) => {
   const query = req.query.f
-  const url = `https://api.edamam.com/search?q=${ query }&app_id=${ appId }&app_key=${ appKey }&from=0&to=3&calories=gte%20591,%20lte%20722&health=alcohol-free`
+  const uri = `https://api.edamam.com/search?q=${ query }&app_id=${ appId }&app_key=${ appKey }&from=0&to=3&calories=gte%20591,%20lte%20722&health=alcohol-free`
 
-  request(url, function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  res.send(body);
-});
-  // .then(recipe => res.send(recipe.toString()))
-  // .catch(err => console.log(err))
+request(uri)
+  .then(process => res.send(process))
+  .catch(console.log)
+
+//Below uses request without promises, which has beemn changed
+// *********************************************************
+//   request(url, function (error, response, body) {
+//   console.log('error:', error); // Print the error if one occurred
+//   res.send(body);
+// });
 })
 
 
