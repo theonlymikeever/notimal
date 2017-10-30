d8e1d251678fea1cf679b1d8372774784865453f
Mike Kerslake <theonlymikeever@gmail.com>
2017-10-29 21:45:35 -0400

    UI changes to scorecard + helper funcs

--- client/components/ItemResultCard.js
+++ client/components/ItemResultCard.js
@@ -1,47 +1,44 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { isVegan } from '../helpers'
+import { isVegan, toTitleCase } from '../helpers'
 
 
 const ItemResultCard = (props) => {
+  //this needs ot be cleaned up
   const { item } = props
   const name = item && item.ingredients
               ? item.ingredients[0].parsed[0].food
               : 'loading'
+  const ingredients = item && item.ingredients[0].parsed[0].foodContentsLabel
+              ? toTitleCase(item.ingredients[0].parsed[0].foodContentsLabel)
+              : 'N/A'
+  const nutrients = item && item.totalNutrients
+              ? Object.keys(item.totalNutrients).map(key => item.totalNutrients[key])
+              : 'N/A'
   return (
-    <div>
-        <div className="card">
-          <h4 className="card-header">{ name }</h4>
-          <div className="card-body">
-            <h4 className="card-title">
-              { item
-                ? <span><img src={isVegan(item.healthLabels)} width="35" className="mr-1" />{ isVegan(item.healthLabels) ? 'Vegan' : 'Not-Vegan'}</span>
-                : null
-              }
-            </h4>
-            <p className="card-text">{item.length && item.calories}</p>
-            <div className="row">
-            <span>JSON.stringify(item.totalNutrients, null, 2)</span>
-              {
-                item.totalNutrients.map(nutr => {
-                  return (
-                    <div key={nutr.uri} className="card text-white bg-success mb-2">
-                      <div className="card-header">{nutr.label}</div>
-                      <div className="card-body">
-                        <p className="card-text">{nutr.quantity.toFixed(2)}{nutr.unit}</p>
-                      </div>
-                    </div>
-                  )
-                })
-              }
-            </div>
-          </div>
-        </div>
-        <span>full data for build:</span>
-        {
-          item && JSON.stringify(item, null, 2)
+  <div className="card">
+    <h4 className="card-header">{ name }</h4>
+    <div className="card-body">
+      <h4 className="card-title">
+        { item
+          ? <span><img src={isVegan(item.healthLabels)} width="35" className="mr-1" />{ isVegan(item.healthLabels) ? 'Vegan' : 'Not-Vegan'}</span>
+          : null
         }
+      </h4>
+      <p className="card-text">Ingredients: {ingredients}</p>
+      <div className="row">
+        {
+          nutrients.map((nutr, i) => {
+            return (
+              <button key={i} type="button" className="btn btn-success m-1">
+                {nutr.label} <span className="badge badge-light">{nutr.quantity.toFixed(0, 2)}{nutr.unit}</span>
+              </button>
+            )
+          })
+        }
+      </div>
     </div>
+  </div>
   )
 }
 
--- client/helpers.js
+++ client/helpers.js
@@ -269,3 +269,6 @@ export function isVegan (labels){
     }
   }
 
+export function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
