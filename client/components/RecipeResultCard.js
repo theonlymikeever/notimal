b31046ab3af1202a9b3e3cc3bd0f9dd4fc59d2ae
Mike Kerslake <theonlymikeever@gmail.com>
2017-10-31 13:25:04 -0400

    Formatting

--- client/components/RecipeResultCard.js
+++ client/components/RecipeResultCard.js
@@ -5,21 +5,29 @@ import { isVegan } from '../helpers'
 const RecipeResultCard = (props) => {
   console.log('props:',props)
   return (
-  <div>
-  <ul>
+  <table className="table table-striped table-success">
+    <thead>
+      <tr>
+        <th>Recipe</th>
+        <th>Vegan</th>
+      </tr>
+    </thead>
   {
     props.recipe && props.recipe.hits
       ? props.recipe.hits.map((res, i) => {
         let vegan = isVegan(res.recipe.healthLabels)
-        console.log(vegan)
         return (
-          <li key={i}>{ res.recipe.label } - <img src={ vegan ? 'images/broccoli.png' : '/images/steak.png'} width="45" className="mr-1" />{ vegan ? 'Vegan' : 'Not-Vegan'}</li>
+        <tbody key={i}>
+          <tr>
+            <th>{ res.recipe.label }</th>
+            <td><img src={ vegan ? 'images/broccoli.png' : '/images/steak.png'} width="45" className="mr-1" />{ vegan ? 'Vegan' : 'Not-Vegan'}</td>
+          </tr>
+      </tbody>
         )
       })
       : null
   }
-  </ul>
-  </div>
+  </table>
   )
 }
 
